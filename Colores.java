b7bbac289eb437d062cb6a68615b0e91e27e69e9
gianella <202400899f@utea.edu.pe>
2025-05-15 09:50:25 -0700

    trabajos

---
+++ Colores.java
@@ -0,0 +1,10 @@
+public class Colores {
+
+    public static void main(String[] args) {
+        System.out.println("\033[ 33m mandarina");
+        System.out.println("\033[32m verde");
+        System.out.println("\033[31m tomate");
+        System.out.println("\033[36m cielo");
+        System.out.println("\033[34m mar");
+    }
+}
