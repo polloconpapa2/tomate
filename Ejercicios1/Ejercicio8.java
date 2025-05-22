013d40a2cda2477e69b108b78ebfcea9edcc15b3
gianella <202400899f@utea.edu.pe>
2025-05-22 09:52:46 -0700

    conejo

--- Ejercicios1/Ejercicio8.java
+++ Ejercicios1/Ejercicio8.java
@@ -2,17 +2,9 @@ package Ejercicios1;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("             (\\(\\        ");
+        System.out.println("             ( -.-)       ");
+        System.out.println("           o_(\")(\")          ");
         
     }
     
