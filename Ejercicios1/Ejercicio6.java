6616a82ff0beb7e74870f35c1568567324488606
gianella <202400899f@utea.edu.pe>
2025-05-22 09:39:39 -0700

    tarea

---
+++ Ejercicios1/Ejercicio6.java
@@ -0,0 +1,12 @@
+package Ejercicios1;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("  *******  ");
+        System.out.println(" ********* ");
+    }
+    
+}
---
+++ Ejercicios1/Ejercicio8.java
@@ -0,0 +1,19 @@
+package Ejercicios1;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        
+    }
+    
+}
