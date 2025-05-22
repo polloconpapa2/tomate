1ea7e9cdeaac94b5a429a766680cf24475c625c9
gianella <202400899f@utea.edu.pe>
2025-05-22 08:37:17 -0700

    horario

---
+++ Ejercicios1/Ejercicio4.java
@@ -0,0 +1,28 @@
+package Ejercicios1;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|   hora    |    Lunes     |    Martes    | Miercoles     |    Jueves   |  Viernes |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|   8:00    | matematica   |     ARTE     |   ingles      |  filosofia  | computo  |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|   9:00    | matematica   |     ARTE     |   ingles      |  filosofia  | computo  |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  10:00    | comunicacion |    musica    |  comunicacion |   musica    | ingles   |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  11:00    | comunicacion |    musica    |  comunicacion |   musica    | ingles   |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  12:00    |                              RECREO                                  |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  13:00    |   C Y T      |  filosofia   |    C Y T      |    DPCC     |C Y T     |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  14:00    |   C Y T      |  filosfia    | matematica    |    DPCC     |C Y T     |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  15:00    |   DPCC       |  filosofia   | matematica    |    ARTE     |C Y T     |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+        System.out.println("|  16:00    |   DPCC       |   computo    | matematica    |    ARTE     | DPCC     |");
+        System.out.println("|----------------------------------------------------------------------------------|");
+    }
+    
+}
